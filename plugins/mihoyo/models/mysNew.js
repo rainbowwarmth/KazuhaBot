bc378fa4118382e17152b2b9c9fd3a0048c0c5b5
rainbowwarmth <94736026+rainbowwarmth@users.noreply.github.com>
2024-11-21 15:12:15 +0000

    Deploying to build from  @ 98c9afc140e6ce62d287b61afd2b9db483f3ff88 🚀

--- plugins/mihoyo/models/mysNew.js
+++ plugins/mihoyo/models/mysNew.js
@@ -8,10 +8,13 @@ exports.miGetPostFull = miGetPostFull;
 const node_fetch_1 = __importDefault(require("node-fetch"));
 const logger_1 = __importDefault(require("../../../lib/logger"));
 async function miGetNewsList(gid, type, pageSize = 10) {
-    return (0, node_fetch_1.default)(`https://bbs-api-static.miyoushe.com/painter/wapi/getNewsList?gids=${gid}&page_size=${pageSize}&type=${type}`, {
+    return (0, node_fetch_1.default)(`https://bbs-api-static.miyoushe.com/painter/wapi/getNewsList?client_type=4&gids=${gid}&last_id=&page_size=${pageSize}&type=${type}`, {
         method: "GET",
-        headers: { Referer: 'https://www.miyoushe.com',
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36 Edg/130.0.0.0' }
+        headers: {
+            Referer: 'https://www.miyoushe.com',
+            origin: 'https://www.miyoushe.com',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36 miHoYoBBS/2.77.1'
+        }
     }).then(res => {
         return res.json();
     }).then((json) => {
@@ -27,8 +30,11 @@ async function miGetNewsList(gid, type, pageSize = 10) {
 async function miGetPostFull(gid, postId) {
     return (0, node_fetch_1.default)(`https://bbs-api.miyoushe.com/post/wapi/getPostFull?gids=${gid}&read=1&post_id=${postId}`, {
         method: "GET",
-        headers: { Referer: 'https://www.miyoushe.com',
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36 Edg/130.0.0.0' }
+        headers: {
+            Referer: 'https://www.miyoushe.com',
+            origin: 'https://www.miyoushe.com',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36 miHoYoBBS/2.77.1'
+        }
     }).then(res => {
         return res.json();
     }).then((json) => {
