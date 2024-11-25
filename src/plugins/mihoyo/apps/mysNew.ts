08a35bfb6e3111302ef20e6e35f478caf9f9de97
rainbowwarmth <3445487890@qq.com>
2024-11-25 13:41:17 +0800

    fix: 细节优化

--- src/plugins/mihoyo/apps/mysNew.ts
+++ src/plugins/mihoyo/apps/mysNew.ts
@@ -294,6 +294,34 @@ export async function taskPushNewsForGame(gid: number) {
     logger.debug(`${gameName} 官方公告检查完成`);
 }
 
+export async function bbbtaskPushNews() {
+    await taskPushNewsForGame(1);
+}
+
+export async function ystaskPushNews() {
+    await taskPushNewsForGame(2);
+}
+
+export async function bbtaskPushNews() {
+    await taskPushNewsForGame(3);
+}
+
+export async function wdtaskPushNews() {
+    await taskPushNewsForGame(4);
+}
+
+export async function dbytaskPushNews() {
+    await taskPushNewsForGame(5);
+}
+
+export async function srtaskPushNews() {
+    await taskPushNewsForGame(6);
+}
+
+export async function zzztaskPushNews() {
+    await taskPushNewsForGame(8);
+}
+
 export async function detalData(data: PostFullPost) {
     var json;
     try {
